664e1d83360a141bc1c035e653cd7190a89cf397
mutcdev <murilotc.dev@gmail.com>
2026-05-05 16:24:06 -0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -9,6 +9,17 @@ const pokemonType = document.querySelector('.pokemon_type')
 const pokemonHeight = document.querySelector('.pokemon_height')
 const pokemonWeight = document.querySelector('.pokemon_weight')
 const pokemonAblities = document.querySelector('.pokemon_ablities')
+const btnFavorite = document.querySelector('.btn_favorite')
+const btnViewFav = document.querySelector('.btn_viewFav')
+
+const statBars = {
+    hp: document.querySelector('.hp'),
+    attack: document.querySelector('.attack'),
+    defense: document.querySelector('.defense'),
+    "special-attack": document.querySelector('.spAttack'),
+    "special-defense": document.querySelector('.spDefense'),
+    speed: document.querySelector('.speed')
+}
 
 const typeIcons = {
     normal: '⚪',
@@ -32,6 +43,13 @@ const typeIcons = {
 }
 
 let currentId = 25;
+let favorites = JSON.parse(localStorage.getItem('favorite')) || []
+let indexFavs = 0
+let favMode = false
+let lastPoke = 25
+
+btnFavorite.addEventListener('click', favoritePoke)
+btnViewFav.addEventListener('click', viewFavorites)
 
 async function searchPokemon(pokemon){
     try{
@@ -46,7 +64,13 @@ async function searchPokemon(pokemon){
 
         currentId = data.id;
 
-        pokemonImage.src = data.sprites.versions['generation-v']['black-white'].animated.front_default
+        const gif = pokemonImage.src = data.sprites.versions['generation-v']['black-white'].animated.front_default
+
+        if (gif){
+            pokemonImage.src=gif
+        }else{
+            pokemonImage.src=data.sprites.front_default
+        }
         pokemonNumber.textContent = `#${data.id}`
         pokemonName.textContent = data.name
         
@@ -59,25 +83,108 @@ async function searchPokemon(pokemon){
 
         pokemonHeight.textContent = data.height / 10
         pokemonWeight.textContent = data.weight / 10
+
+        if (favorites.includes(data.name)){
+            btnFavorite.classList.add('active')
+        }else{
+            btnFavorite.classList.remove('active')
+        }
+        if (!favMode){
+            lastPoke = data.id
+        }
+
+        data.stats.forEach(stat => {
+            const nameS = stat.stat.name
+            const value = stat.base_stat
+
+            const percent = (value/255) * 100
+
+            if (statBars[nameS]){
+                statBars[nameS].style.width = percent + '%'
+                statBars[nameS].style.background = getColor(value)
+            }
+        })
+        pokemonAblities.textContent=''
+
+        data.abilities.forEach(a => {
+            const nameA = a.ability.name
+            pokemonAblities.textContent += nameA + ' • '
+        })
     }
     catch (error){
         pokemonName.textContent='Error'
-        console.log('Erro: ', error)
+        console.log('Error: ', error)
     }
 }
 
+function getColor(value){
+    if (value < 50) return 'red'
+    if (value <100) return 'orange'
+    if (value <150) return 'yellow'
+    return 'rgb(7, 228, 198)'
+}
+
+function favoritePoke(){
+    const name = pokemonName.textContent
+    
+    if (favorites.includes(name)){
+        favorites = favorites.filter(f => f !== name)
+        btnFavorite.classList.remove('active')
+    }else{
+        favorites.push(name)
+        btnFavorite.classList.add('active')
+    }
+    
+    localStorage.setItem('favorite', JSON.stringify(favorites))
+    
+}
+
+function viewFavorites(){
+    if (favMode){
+        favMode = false
+        btnViewFav.textContent = 'View Favorites'
+        searchPokemon(lastPoke)
+    }else{
+        if (favorites.length > 0){
+        favMode=true
+        indexFavs=0
+        btnViewFav.textContent = 'Exit Favorites'
+        searchPokemon(favorites[0])
+        }else{
+            alert('No favorites yet')
+        }
+    }
+    
+    
+}
+
 form.addEventListener('submit', (event) => {
+    favMode=false
     event.preventDefault()
     const value=input.value.toLowerCase().trim()
     if (value) searchPokemon(value)
 })
 
 btnPrev.addEventListener('click', () => {
-    if (currentId>1) searchPokemon(currentId-1)
+    if (favMode){
+        if (indexFavs > 0){
+            indexFavs--
+            searchPokemon(favorites[indexFavs])}
+    }else{
+        if (currentId>1) searchPokemon(currentId-1)
+    }
 })
 
 btnNext.addEventListener('click', () => {
+    if (favMode){
+        if (indexFavs < favorites.length -1){
+            indexFavs++
+            searchPokemon(favorites[indexFavs])}
+    }else{
     searchPokemon(currentId+1)
+    }
 })
+
+
 
 searchPokemon(25)
